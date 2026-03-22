772f14c6935fc6208bb4e9bed03eca66f7f960f8
jayakrishna20 <jayakrishnans2041@gmail.com>
2026-03-22 13:27:04 +0530

    feat(cli): dynamically bind version based on github tags

--- cmd/root.go
+++ cmd/root.go
@@ -7,14 +7,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var Version string
+
 var rootCmd = &cobra.Command{
 	Use:     "avtr",
-	Version: "1.0.0",
 	Short:   "Avatar: A powerful CLI tool for PDF and Image manipulations",
 	Long:    `Avatar (avtr) provides lightning-fast operations to split, merge, and convert PDFs and Images.`,
 }
 
 func Execute() {
+	rootCmd.Version = Version
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "avtr/cmd"
 
+var version = "dev" // This gets seamlessly overwritten by GoReleaser during the build
+
 func main() {
+	cmd.Version = version
 	cmd.Execute()
 }
